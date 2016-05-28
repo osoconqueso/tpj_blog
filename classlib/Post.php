0d22d1321b62da11ca347b3f7db795890548db8c
Jessica Hopper <jessica.hopper@gmail.com>
2016-05-27 19:39:26 -0500

    Adding class lib

---
+++ classlib/Post.php
@@ -0,0 +1,69 @@
+<?php
+
+class Post {
+
+    /**
+     * @var string
+     * title of post
+     */
+    public $title;
+
+    /**
+     * @var string
+     * date posted
+     */
+    public $date;
+
+    /**
+     * @var string
+     * author of post
+     */
+    public $author;
+
+    /**
+     * @var string
+     * post content
+     */
+    public $content;
+
+    /**
+     * @var string
+     * image source
+     */
+    public $img;
+
+    /**
+     *
+     *
+     */
+    public function construct() {
+        $this->title = $title;
+        $this->date = $date;
+        $this->author = $author;
+    }
+
+    public function createPost() {
+
+    }
+
+    public function editPost() {
+
+    }
+
+    public function updatePost() {
+
+    }
+
+    public function deletePost() {
+
+    }
+
+    public function getPost() {
+
+    }
+
+    public function printPost() {
+
+    }
+
+}
---
+++ classlib/User.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jessi_000
+ * Date: 5/26/2016
+ * Time: 8:52 PM
+ */
