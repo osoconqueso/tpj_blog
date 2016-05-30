6914a86c94df4f23f476b98aef21bf78689804c4
Jessica Hopper <jessica.hopper@gmail.com>
2016-05-30 19:02:23 +0000

    added db connection ith '#' will be ignored, and an empty message aborts the commit.

--- classlib/Mysql.php
+++ classlib/Mysql.php
@@ -1,7 +1,41 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jessi_000
- * Date: 5/26/2016
- * Time: 8:52 PM
- */
+
+class Mysql {
+
+    /**
+     * instantiated instance of the object
+     * @var MySQL database
+     */
+
+    private static $instance;
+
+    public $db;
+
+    public function construct() {
+
+        $hostName = '127.0.0.1';
+        $userName = 'root';
+        $password = 'root';
+        $database = 'tpj';
+
+        //change this later to PDO::ERRMODE_SILENT
+        //ATTR_ERRMODE => PDO::ERRMODE_WARNING gives php error
+        //change to PDO::ERRMODE_EXCEPTION  - when handling the errors with 'try'{} $ 'catch'{}
+
+        $this->db = new PDO("mysql:host=$hostName;dbname=$database", $userName, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+
+        /**
+         * Get a singleton instance of the database
+         * @return Mysql database
+         */
+        public static function getInstance()
+        {
+            if (!isset(self::$instance)) {
+                self::$instance = new self();
+            }
+
+            return self::$instance;
+        }
+
+    }
+}
