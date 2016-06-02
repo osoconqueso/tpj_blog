519c8e356b15f1921063646b3f9f6f4410a0a1a2
Tracy Thompson <traymarkthompson@gmail.com>
2016-06-01 23:26:30 -0500

    deleted  bootstrap3  links corrected main.css relative reference moved navigation stuff under toggle. TODO...slide right? What navigation items? move search elsewhere TODO integrate responsive background from  tpj4 on traycycles github acct.

--- index.php
+++ index.php
@@ -8,13 +8,8 @@ include("functions.php");
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!--"shrink-to-fit=no" can be added below after "initial scale=1" for Safari 9.0 users to override behaviour that scales down the page-->
    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-   <!--    <link type="text/css" rel="stylesheet" href="blogstyle.css"/>-->
    <title>tpj blog</title>
-   <link rel="stylesheet" type="text/css" href="main.css">
-   
-   <!-- Latest compiled and minified CSS -->
-  <!-- <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">-->
-   
+   <link rel="stylesheet" type="text/css" href="CSS/main.css">
    <!-- Bootstrap 4 CDN link -->
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
    
@@ -24,12 +19,24 @@ include("functions.php");
 
 <body>
 <div>
-    <div class="container-fluid">
+<!--padding-top-1 rem = p-t-1 -->
+    <div class="container-fluid p-t-1 bg">
         <div class="row">
             <div class="collapse" id="exCollapsingNavbar">
-                <div class="bg-inverse p-a-1">
-                    <h4>Menu</h4>
-                    <span class="text-muted">Toggleabled crap here. Slide? </span>
+                <div class="p-a-1">
+                    <a href="index.php">TPJ</a>
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="index.php">Home</a></li>
+                            <li><a href="#">blogs</a></li>
+                            <li><a href="#">reports</a></li>
+                            <li><a href="#">sign-up</a></li>
+                            <li><span class="glyphicon glyphicon-search"><input type="text" placeholder="Search" /></li>
+                            <li><a href="#">about</a></li>
+                            <li><a href="#">support us</a></li>
+                            <li><a href="#">contact</a></li>
+                        </ul>
+<!--                    <h4>Menu</h4>-->
+<!--                    <span class="text-muted">Toggleabled crap here. Slide? </span>-->
                 </div>
             </div>
             <nav class="navbar navbar-light">
@@ -41,28 +48,28 @@ include("functions.php");
         </div>
 </div>
 
-   <div class="container">
-
-      <div class="navbar navbar-inverted">
-         <div class="container-fluid">
-            <div class="navbar-header">
-               <a class="navbar-brand" href="index.php">TPJ</a>
-            </div>
-            <ul class="nav navbar-nav">
-               <li class="active"><a href="index.php">Home</a></li>
-               <li><a href="#">blogs</a></li>
-               <li><a href="#">reports</a></li>
-               <li><a href="#">sign-up</a></li>
-               <li><span class="glyphicon glyphicon-search"><input type="text" placeholder="Search" /></li>
-               <li><a href="#">about</a></li>
-               <li><a href="#">support us</a></li>
-               <li><a href="#">contact</a></li>
-            </ul>
-            </div>
-         </div>
-      </div>
+<!--   <div class="container">-->
+<!---->
+<!--      <div class="navbar navbar-inverted">-->
+<!--         <div class="container-fluid">-->
+<!--            <div class="navbar-header">-->
+<!--               <a class="navbar-brand" href="index.php">TPJ</a>-->
+<!--            </div>-->
+<!--            <ul class="nav navbar-nav">-->
+<!--               <li class="active"><a href="index.php">Home</a></li>-->
+<!--               <li><a href="#">blogs</a></li>-->
+<!--               <li><a href="#">reports</a></li>-->
+<!--               <li><a href="#">sign-up</a></li>-->
+<!--               <li><span class="glyphicon glyphicon-search"><input type="text" placeholder="Search" /></li>-->
+<!--               <li><a href="#">about</a></li>-->
+<!--               <li><a href="#">support us</a></li>-->
+<!--               <li><a href="#">contact</a></li>-->
+<!--            </ul>-->
+<!--            </div>-->
+<!--         </div>-->
+<!--      </div>-->
       <h3>TPJ blog</h3>
-      
+
       <div>
          <ul class="nav">
               <li class="sort-swap"><a href="index.php" title="sort by...">sort by</a>
@@ -74,8 +81,8 @@ include("functions.php");
               <li class="newpost-swap"><a href="post_article.php" title="post new article">new post</a></li>
           </ul>
       </div>
-      
-      
+
+
       <div id = "content">
           <?php
       
@@ -95,12 +102,7 @@ include("functions.php");
       <!--</div>-->
    </div>
    <!-- jQuery library -->
-<!--   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>-->
-<!--   -->
-<!--   <!-- Latest compiled JavaScript -->-->
-<!--   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>-->
-<!--   -->
-   <!--Bootstrap 4 JavaScript-->
+<!--Bootstrap 4 JavaScript-->
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
 </body>
