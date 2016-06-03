d851d4f1ad9f89cd5d625cf3212350c34e848c50
tracy <traymarkthompson@gmail.com>
2016-06-03 15:09:55 -0500

    commented out PHP... connex timeout causing toggle issues. added new section id/containers for blogs and placeholder old HTML in section.

--- index.php
+++ index.php
@@ -4,16 +4,16 @@
 
 <html lang="en">
 <head>
-   <meta charset="utf-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!--"shrink-to-fit=no" can be added below after "initial scale=1" for Safari 9.0 users to override behaviour that scales down the page-->
-   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-   <title>tpj blog</title>
-   <!-- Bootstrap 4 CDN link -->
-   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>tpj blog</title>
+    <!-- Bootstrap 4 CDN link -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
     <!-- custom css   -->
-   <link rel="stylesheet" href="CSS/main.css">
-   
+    <link rel="stylesheet" href="CSS/main.css">
+
 </head>
 
 <body>
@@ -46,8 +46,8 @@
         <p id="signup-btn">
             <a href="" class="btn btn-md btn-info" style="display: inline">Sign up</a>
         </p>
-         <div class="input-group" id="emailsignup">
-                <input type="text" class="form-control" placeholder="Email">
+        <div class="input-group" id="emailsignup">
+            <input type="text" class="form-control" placeholder="Email">
       <span class="input-group-btn">
         <button class="btn btn-info" type="button">Sign up!</button>
       </span>
@@ -56,44 +56,52 @@
 
     </div>
 </div>
-
-<div style="padding-top: 2rem">
-      <h3>TPJ blog</h3>
-
-      <div>
-         <ul class="nav">
-              <li class="sort-swap"><a href="index.php" title="sort by...">sort by</a>
-                  <ul class="dropdown">
-                      <li><a href="index.php?sort=id ASC">sort by date</a></li>
-                      <li><a href="index.php?sort=title ASC">sort by show</a></li>
-                  </ul>
-              </li>
-              <li class="newpost-swap"><a href="post_article.php" title="post new article">new post</a></li>
-          </ul>
-      </div>
+<section id="homeblogs">
+    <!--  container-fluid is for full width  container, spanning entire viewport width  -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="feature-content">
+                <h3>TPJ blog</h3>
+                <div>
+                    <ul class="nav">
+                        <li class="sort-swap"><a href="index.php" title="sort by...">sort by</a>
+                            <ul class="dropdown">
+                                <li><a href="index.php?sort=id ASC">sort by date</a></li>
+                                <li><a href="index.php?sort=title ASC">sort by show</a></li>
+                            </ul>
+                        </li>
+                        <li class="newpost-swap"><a href="post_article.php" title="post new article">new post</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
+</section>
 
 
-      <div id = "content">
-          <?php
-      
-          if (isset($_GET['sort'])) {
-              $sort = $_GET['sort'];
-              print_all_articles($sort);
-          } else {
-              print_all_articles();
-          }
-      
-          ?>
-      
-      </div>
-      <!--<div>-->
-      <!--    <p>Color palette brought to you by <a href="http://pokepalettes.com/#geodude">Geodude</a></p>-->
-      <!--    <p>Summaries from <a href="http://imdb.com">IMDB</a></p>-->
-      <!--</div>-->
-   </div>
-   <!-- jQuery library followed by Bootstrap JS -->
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+
+
+
+<div id = "content">
+<!--    --><?php
+//
+//    if (isset($_GET['sort'])) {
+//        $sort = $_GET['sort'];
+//        print_all_articles($sort);
+//    } else {
+//        print_all_articles();
+//    }
+//
+//    ?>
+
+</div>
+<!--<div>-->
+<!--    <p>Color palette brought to you by <a href="http://pokepalettes.com/#geodude">Geodude</a></p>-->
+<!--    <p>Summaries from <a href="http://imdb.com">IMDB</a></p>-->
+<!--</div>-->
+</div>
+<!-- jQuery library followed by Bootstrap JS -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
 </body>
 </html>
